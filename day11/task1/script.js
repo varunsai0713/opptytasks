08279206fc8e3466ce13de7d2b33dcb2c41ea8da
varunsai0713 <142675197+varunsai0713@users.noreply.github.com>
2025-11-25 21:56:04 +0530

    p

---
+++ day11/task1/script.js
@@ -0,0 +1,11 @@
+// using variable declartions
+let name = "Varun";
+let id = 60;
+let address = "andhrapradesh";
+let hobbies = "playing";
+let hobies = "singing";
+console.log(name);
+console.log(id);
+console.log(address);
+console.log(hobbies);
+console.log(hobies);
