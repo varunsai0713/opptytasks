1e0219bc669e591271f9989adef6017a59a4164c
varunsai0713 <saivarun0713@gmail.com>
2026-01-02 22:52:15 +0530

    p

--- netflix/script.js
+++ netflix/script.js
@@ -1,34 +1,7 @@
-
-
-const trending = [
-  "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
-  "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-  "https://image.tmdb.org/t/p/w500/lNyLSOKMMeUPr1RsL4KcRuIXwHt.jpg"
-];
-
-const popular = [
-  "https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
-  "https://image.tmdb.org/t/p/w500/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg",
-  "https://image.tmdb.org/t/p/w500/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg"
-];
-
-function loadMovies(list, id){
-  const container = document.getElementById(id);
-  list.forEach(img=>{
-    const div = document.createElement("div");
-    div.className = "card";
-    div.innerHTML = `<img src="${img}">`;
-    container.appendChild(div);
-  });
-}
-
-loadMovies(trending, "trending");
-loadMovies(popular, "popular");
-
-
 const API_KEY = "ee9aa55b";
 const API_URL = "https://www.omdbapi.com/";
 
+/* SEARCH ON ENTER */
 document.getElementById("searchInput").addEventListener("keydown", function(e){
   if(e.key === "Enter"){
     searchSeries();
@@ -48,20 +21,21 @@ function searchSeries(){
     return;
   }
 
+  container.innerHTML = `<div class="loader"></div>`;
+
   fetch(`${API_URL}?t=${seriesName}&type=series&apikey=${API_KEY}`)
     .then(res => res.json())
     .then(data => {
-
       if(data.Response === "False"){
+        container.innerHTML = "";
         message.textContent = "No TV series found";
         return;
       }
 
       container.innerHTML = `
-        <div class="series-card">
-          <img src="${data.Poster !== "N/A" ? data.Poster : ""}">
+        <div class="result-card">
           <h2>${data.Title}</h2>
-          <p class="series-rating">⭐ IMDb Rating: ${data.imdbRating}</p>
+          <p>⭐ IMDb Rating: ${data.imdbRating}</p>
           <p><strong>Total Seasons:</strong> ${data.totalSeasons}</p>
           <p>${data.Plot}</p>
         </div>
